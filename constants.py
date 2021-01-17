2409bb81242412c7fe2c833b51daa68e76077272
JustAnyone <dom.svetikas@gmail.com>
2021-01-17 14:13:48 +0200

    Add constants file

---
+++ constants.py
@@ -0,0 +1,46 @@
+PIDROID_ID = 700465365435678840
+JUSTANYONE_ID = 333871512496898059
+
+THEOTOWN_GUILD = 365478391719264276
+
+MINECRAFT_LISTENER_GUILD = 623839265238810624
+MINECRAFT_LISTENER_CHANNEL = 767930263052746822
+MINECRAFT_LISTENER_USER = 720001048210571275
+
+TEMPORARY_FILE_PATH = './data/temporary/'
+
+FACTS = [
+    'Javascript is trash.\n\n Source: dude, just trust me',
+    'RemainsStay is our lovely sloth admin',
+    'Ene7 is our detective pingu',
+    'Evan is our well known firetruck',
+    'Lobby is an entranceway or foyer in a building, idk, that\'s what Google search returned',
+    'JustAnyone is just erksmit in disguise',
+    'The26 likes ducks',
+    'Someone who starts with a letter Q has an ultrawide wallpaper obsession, though I am not pointing any fingers, yet, that is.',
+    'KolaKattz is a funny way of saying cat enjoying bepis',
+    'I have multiple brothers, one of them is named Pidroid Canary. Perhaps you\'ve met him already?',
+    'This bot is built using discord.py wrapper made by an awesome guy called Rapptz',
+    'Do you have any medic bags by any chance?',
+    'Yes, *kentucky* fried chicken is indeed my good friend',
+    'Did I ever tell you the definition of insanity? No? Welp, I can\'t just do it now!',
+    'cheesyLord sounds pretty cheesy, who would\'ve guessed',
+    'Lucas made an amazing soundtrack for the game',
+    'NOLOGIC? ah, it is a secret gamemode which is broken in every way possible. It very familiar to sandbox mode.',
+    'This feature was added in Pidroid 2.8.0',
+    'January 15th is Fire Truck Appreciation Day'
+]
+REFUSE_COMMAND_RESPONSES = [
+    'No',
+    'I don\'t think so!',
+    'Horrible',
+    'Just what do you think you\'re doing?',
+    'I apologise, however I won\'t do that'
+]
+
+
+def setup(client):
+  print('Constants loaded')
+
+def teardown(client):
+  print('Constants unloaded')
