1ee18f92c33906401e265f4f8226e7bc721d7caa
JustAnyone <dom.svetikas@gmail.com>
2021-02-22 13:26:51 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 PIDROID_ID = 700465365435678840
+PIDROID_CANARY_ID = 762365831018577980
 JUSTANYONE_ID = 333871512496898059
 
 THEOTOWN_DEVELOPERS = [
@@ -6,49 +7,85 @@ THEOTOWN_DEVELOPERS = [
     364343400461238272 # Lobby
 ]
 
-RCON_OPERATORS = [
+# Authorized users who can shut down the bot in case of emergency
+# TODO: implement
+CRITICAL_SHUTDOWN_USERS = [
+    THEOTOWN_DEVELOPERS,
+    329729373441884172 # Remains
+]
+
+CHEESE_EATERS = [
     JUSTANYONE_ID,
-    444874161911758849 # KolaKattz
+    308903727928967169
 ]
 
 THEOTOWN_GUILD = 365478391719264276
 
+# Theocraft minecraft server
 MINECRAFT_LISTENER_GUILD = 623839265238810624
 MINECRAFT_LISTENER_CHANNEL = 767930263052746822
 MINECRAFT_LISTENER_USER = 720001048210571275
 
-TEMPORARY_FILE_PATH = './data/temporary/'
+# Pidroid file structure
+DATA_FILE_PATH = './data/'
+CACHE_FILE_PATH = DATA_FILE_PATH + 'cache.json'
+TEMPORARY_FILE_PATH = DATA_FILE_PATH + 'temporary/'
 
+# Used for fact command
 FACTS = [
-    'Javascript is trash.\n\n Source: dude, just trust me',
-    'RemainsStay is our lovely sloth admin',
-    'Ene7 is our detective pingu',
-    'Evan is our well known firetruck',
-    'Lobby is an entranceway or foyer in a building, idk, that\'s what Google search returned',
-    'JustAnyone is just erksmit in disguise',
-    'The26 likes ducks',
-    'Someone who starts with a letter Q has an ultrawide wallpaper obsession, though I am not pointing any fingers, yet, that is.',
-    'KolaKattz is a funny way of saying cat enjoying bepis',
+    'Javascript is isn\'t the greatest programming language in the world, but at least it counts its arrays from 0 unlike some other languages...',
+    'RemainsStay is a lovely sloth administrator of TheoTown server.',
+    'Ene7 is our detective pingu.',
+    'Evan? Oh, you mean that helpful firetruck. Yeah, he\'s awesome.',
+    'Lobby is an entranceway or foyer in a building. What? That\'s not what Lobby is?',
+    'I\'ve heard a user by the name of The26 likes ducks. I suggest you send them a duck picture.',
+    'Yo, Q||_REMOVED_||02, do you know where I can get some of them wide monitors you have?',
+    'KolaKattz? Nope, never heard of him. Have you?',
     'I have multiple brothers, one of them is named Pidroid Canary. Perhaps you\'ve met him already?',
-    'This bot is built using discord.py wrapper made by an awesome guy called Rapptz',
-    'Do you have any medic bags by any chance?',
+    'This bot is built using discord.py wrapper made by an awesome guy called Rapptz.',
+    'Man, the other day I was searching for some medic bags and I couldn\'t find any. They are all gone!',
     'Yes, *kentucky* fried chicken is indeed my good friend',
     'Did I ever tell you the definition of insanity? No? Welp, I can\'t just do it now!',
-    'cheesyLord sounds pretty cheesy, who would\'ve guessed',
-    'Lucas made an amazing soundtrack for the game',
+    'Lucas King is a great musician with the great soundtrack he made for the game.',
     'NOLOGIC? ah, it is a secret gamemode which is broken in every way possible. It very familiar to sandbox mode.',
-    'This feature was added in Pidroid 2.8.0',
-    'January 15th is Fire Truck Appreciation Day'
+    'This fact feature was introduced in version 2.8.0 of Pidroid, with later versions expanding the facts.',
+    'January 15th is Fire Truck Appreciation Day!',
+    'Jesse does indeed like to talk, he doesn\'t like starting the conversation though.',
+    'The name of DSA agency has no meaning.',
+    'The game is currently being developed by 2 people.',
+    'The name Theo comes from a previous developer of the game called theotheoderich.',
+    'Ever wanted to reset your in-game progress and get an achievement for that? Just open the developer console of the game and run ``rm -rf /theotown``'
 ]
 
+AUTOMODERATOR_RESPONSES = [
+    '%user%, can you be more responsible with your dictionary?',
+    'Looks like you\'re going to the shadow realm for that language, %user%',
+    '%user%, watch your language!',
+    '%user%, please refrain from using such language!'
+]
+
+# Used for responding to suggest command outside of bot-commands channel
 REFUSE_COMMAND_RESPONSES = [
-    'No',
-    'I don\'t think so!',
-    'Horrible',
-    'Just what do you think you\'re doing?',
-    'I apologise, however I won\'t do that'
+    'I do not recommend using my commands here. Perhaps there\'s a better channel for that?',
+    'It wouldn\'t be wise to run your command here, therefore, I won\'t.',
+    'You may not run your command here.',
+    'Sorry, I cannot do that here.'
 ]
 
+# Used for beg command of the economy cog
+BEG_SUCCESS_RESPONSES = [
+    'Ugh, fine, I guess you are my little pogchamp, come here, I\'ll give you %cash%.',
+    'I am feeling nice today, so here, have a handsome sum of %cash%!',
+    'Pssst, have some %cash%, because you\'re so nice to talk to!',
+    'Your prayers have been answered. You may now have %cash%!'
+]
+
+BEG_FAILURE_RESPONSES = [
+    'I wish I could give you some shinies, but I won\'t.',
+    'Congratulations, you did beg me for the shiny theons, however I do not feel generous this time.',
+    'I apologize, I gave my funds to someone else. Maybe next time?',
+    'Uh, oh, I have an important meeting to attend. Sorry, I\'ll catch you later.'
+]
 
 def setup(client):
   print('Constants loaded')
